eceb4a5d67a7f56a765d2076bcbc6ad51b587937
Ervina Anggraeni <ervina@41studio.com>
2015-09-04 10:23:05 +0700

    fix smtp credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'http://ervinaruki.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://ervinaruki.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
@@ -92,8 +92,8 @@ Rails.application.configure do
 
   config.middleware.use ExceptionNotification::Rack,
     :email => {
-      :email_prefix => "[Something went wrong]",
-      :sender_address => %{"Rukiimoera Error ><" <rukiimoera@notifier.com>},
+      :email_prefix => "[Something went wrong] ",
+      :sender_address => %{"Rukiimoera Error" <rukiimoera@notifier.com>},
       :exception_recipients => %w{ervina@41studio.com}
     }
 end
